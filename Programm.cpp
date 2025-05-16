162cd4d5d6638b552907d0c290bec0d48e4e41e3
Edwardz <hanchivi39@gmail.com>
2025-05-16 12:17:22 +1000

    Update Programm.cpp

--- Programm.cpp
+++ Programm.cpp
@@ -1,1 +1,35 @@
+#include "splashkit.h"
+#include <Dinosaur.h>
+#include <Obstacle.h>
+
+using namespace std;
+
+const int SCREEN_WIDTH = 800;
+const int SCREEN_HEIGHT = 300;
+const int GROUND_Y = 250;
+
+load_bitmap("A", "Dinoo.png");
+
+int main() {
+
+    Dinosaur dino;
+    vector <Obstacles> obstacle;
+
+    while(!window_close_requested("Dino Dash DX")) {
+        open_window("Dino Dash DX", SCREEN_WIDTH, SCREEN_HEIGHT);
+        load_font("arial", "arial.ttf");
+
+        while (!window_close_requested("Dino Dash DX")) {
+            process_events();
+            clear_screen(COLOR_WHITE);
+
+            dino.update();
+
+            draw_line(COLOR_DARK_GREEN, 0, GROUND_Y, SCREEN_WIDTH, GROUND_Y);
+            dino.draw();
+        }
+    }
+
+
+}
 
