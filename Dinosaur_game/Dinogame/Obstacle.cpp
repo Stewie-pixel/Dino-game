f9fee01806081f1b16f9f0fa99d902dad5079a75
Edwardz <hanchivi39@gmail.com>
2025-05-16 13:14:30 +1000

    Add files via upload

---
+++ Dinosaur_game/Dinogame/Obstacle.cpp
@@ -0,0 +1,14 @@
+#include "Obstacle.h"
+#include "splashkit.h"
+
+using namespace std;
+
+struct Obstacle_Data {
+    bitmap image;
+    float x, y;
+
+};
+
+Obstacle_Data create_Obstacle() {
+    Obstacle_Data result
+}
