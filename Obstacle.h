f6e6d1d85cd86a5ec971c76b50637b8335828dd5
Edwardz <hanchivi39@gmail.com>
2025-05-16 13:15:22 +1000

    Add files via upload

---
+++ Obstacle.h
@@ -0,0 +1,17 @@
+#pragma once
+#ifndef OBSTACLE_H
+#include "splashkit.h"
+
+using namespace std;
+
+struct Obstacle_Data {
+    sprite cactus;
+    sprite bird;
+    float speed;
+    float x, y;
+};
+
+void create_Obstacle(const Obstacle_Data &obstacle);
+void draw_Obstacle(const Obstacle_Data &obstacle);
+void update_Obstacle(Obstacle_Data &obstacle);
+#endif
